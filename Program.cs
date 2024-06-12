41da484af81cd163af393f7171f325f485b23bc1
David Riddervold Marconis <drmar@equinor.com>
2024-06-12 22:55:23 +0200

    Add dependencies to main Program.cs

--- Program.cs
+++ Program.cs
@@ -1,13 +1,28 @@
+using CleaningService.Data;
+using CleaningService.Services;
+
 public class Program
 {
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        builder.Services.AddControllers();
+        builder.Services.AddControllers(options =>
+        {
+            options.ReturnHttpNotAcceptable = true;
+        });
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
         builder.Services.AddRouting(options => options.LowercaseUrls = true);
+        builder.Services.AddHttpClient();
+        builder.Services.AddSingleton(sp =>
+                {
+                    var configuration = sp.GetRequiredService<IConfiguration>();
+                    var connectionStringOpt = configuration.GetValue<String>("ConnectionString");
+                    var connectionString = connectionStringOpt ?? throw new ArgumentNullException();
+                    return new Database(connectionString);
+                });
+        builder.Services.AddSingleton<NotificationService>();
 
         var app = builder.Build();
         app.Map("/api", api => ConfigureApi(app, api));
